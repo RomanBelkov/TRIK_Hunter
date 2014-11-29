2f0d8127ab781967518752d95faa16b9c3172b11
Roman Belkov <romanbelkov@gmail.com>
2014-11-29 16:59:43 +0300

    Started playing with color mapping

--- Program.fs
+++ Program.fs
@@ -5,10 +5,22 @@ let maxAngleX = 40
 let maxAngleY = 60
 let scaleConstX = 10
 let scaleConstY = 20
-let RGBdepth = 255.
-let SVscale = 100.
+let RGBdepth = 100.
 let minMass = 5
-let deepPink = (255, 20, 147)
+
+let red = (100, 0, 0)
+let green = (0, 100, 0)
+let blue = (0, 0, 100)
+let black = (0, 0, 0)
+let white = (100, 100, 100)
+let brown = (65, 17, 17)
+let orange = (100, 65, 0)
+let yellow = (100, 100, 0)
+let teal = (0, 50, 50)
+let purple = (63, 13, 94)
+let pink = (100, 75, 80)
+let colors = 
+    [red; green; blue; (*white;*) (*black;*) brown; orange; yellow; teal; purple; pink]
 
 let scale var mA sC  = (Trik.Helpers.limit (-mA) mA var) / sC
 
@@ -22,12 +34,25 @@ let updatePositionY y acc =
     then acc + scale y maxAngleY scaleConstY
     else acc
 
+
+let colorProcessor (r, g, b) = 
+    printfn "rgb : %d %d %d" (r * 255 / 100) (g * 255 / 100) (b * 255 / 100)
+    let del (x, y, z) = (x - r) * (x - r) + (g - y) * (g - y) + (b - z) * (b - z)
+    let rec loop (x :: xs) acc =
+        let sup = del x
+        let accDelta = del acc
+        match (x :: xs) with
+        | [t] -> printfn "t : %A ;; acc : %A" t acc; if del t < accDelta then t else acc 
+        | x :: xs when sup < accDelta -> loop xs x
+        | x :: xs when sup >= accDelta -> loop xs acc   
+        | _ -> failwith "no way" 
+    loop colors red
+
 let conversion (x : DetectTarget) = 
-    let h = Trik.Helpers.limit 0.9 1.
-    let (r, g, b) = Trik.Helpers.HSVtoRGB(float x.Hue, h((float x.Saturation) / 100.) , h((float x.Value) / 100.))
-//    printfn "HUE: %d %d %d %d %d %d" x.Hue x.Saturation x.Value x.HueTolerance x.SaturationTolerance x.ValueTolerance
-//    printfn "RGB: %d %d %d" (int (r * RGBdepth)) (int (g * RGBdepth)) (int (b * RGBdepth))
-    (int (r * RGBdepth), int (g * RGBdepth), int (b * RGBdepth))
+    printfn "%d %d %d %d %d %d" x.Hue x.Saturation x.Value x.HueTolerance x.SaturationTolerance x.ValueTolerance
+    let (r, g, b) = 
+        Trik.Helpers.HSVtoRGB(float x.Hue, (float x.Saturation) / 100. , (float x.Value) / 100.)
+    colorProcessor (int (r * RGBdepth), int (g * RGBdepth), int (b * RGBdepth))
 
 let exit = new EventWaitHandle(false, EventResetMode.AutoReset)
 
@@ -40,11 +65,7 @@ let main _ =
     let sensor = model.ObjectSensor
     let buttons = new ButtonPad()
 
-    buttons.Start()
-    sensor.Start()
-
-    Trik.Helpers.SendToShell """v4l2-ctl -d "/dev/video2" --set-ctrl white_balance_temperature_auto=1"""
-    model.LedStripe.SetPower deepPink
+    model.LedStripe.SetPower (75, 20, 20)
 
     let sensorOutput = sensor.ToObservable()
     
@@ -56,14 +77,13 @@ let main _ =
 
     use ledstripeDisposable = colorStream.Subscribe model.LedStripe
 
-    let powerSetterDisposable = 
+    use powerSetterDisposable = 
              model.ObjectSensor.ToObservable()  
              |> Observable.choose (fun o -> o.TryGetLocation)
              |> Observable.filter (fun loc -> loc.Mass > minMass) 
              |> Observable.scan (fun (accX, accY) loc -> (updatePositionX loc.X accX, updatePositionY loc.Y accY)) (0, 0)
              |> Observable.subscribe (fun (a, b) -> model.Servo.["E1"].SetPower -a
                                                     model.Servo.["E2"].SetPower b)
-
 
     use downButtonDispose = buttons.ToObservable() 
                             |> Observable.filter (fun x -> ButtonEventCode.Down = x.Button) 
@@ -75,6 +95,11 @@ let main _ =
 
     use timerSetterDisposable = Observable.Interval(System.TimeSpan.FromSeconds 40.0) 
                                 |> Observable.subscribe (fun _ -> sensor.Detect())
+    
+    buttons.Start()
+    sensor.Start()
+    Trik.Helpers.SendToShell """v4l2-ctl -d "/dev/video2" --set-ctrl white_balance_temperature_auto=1"""
+
 
     exit.WaitOne() |> ignore
     0
